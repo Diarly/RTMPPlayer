8e4eb85872efed03394f5ebc118cd46e0ab1e8ef
Diarly <xuguofei2006@126.com>
2016-05-11 19:21:44 +0800

    Initial Commit

---
+++ RTMPPlayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RTMPPlayer
+//
+//  Created by guofeixu on 16/5/11.
+//  Copyright © 2016年 guofeixu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
